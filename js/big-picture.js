fcad77b996750f8fd9f31e38ee32c86d40725b52
Kirill <89221688844@mail.ru>
2025-11-06 11:41:59 +0500

    8.1.2

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,12 +1,33 @@
-import { getPhotos } from "./data.js";
-import { isEscKey, isEnterKey } from "./utlis.js";
+import { isEscKey, isEnterKey, checkComments } from './utlis.js';
 const bigPicture = document.querySelector('.big-picture');
 const buttonClose = document.querySelector('.big-picture__cancel');
 const bodyBigPicture = document.querySelector('body');
+const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const commentsTotalQuanity = bigPicture.querySelector('.social__comment-total-count');
+const socialCaption = bigPicture.querySelector('.social__caption');
+const commentsShownQuanity = bigPicture.querySelector('.social__comment-shown-count');
+const bigPictureCommentsContainer = bigPicture.querySelector('.social__comments');
+
+
+const renderComments = (comments) => {
+  const fragment = document.createDocumentFragment();
+  comments.forEach((item) => {
+    fragment.push(renderComment(item));
+
+  });
+};
+
 
 const openBigPicture = (photo) => {
   bigPicture.classList.remove('hidden');
   bodyBigPicture.classList.add('modal-open');
+  bigPictureImage.src = photo.url;
+  likesCount.textContent = photo.likes;
+  commentsTotalQuanity.textContent = photo.comments.length;
+  socialCaption.textContent = photo.description;
+  commentsShownQuanity.textContent = checkComments(photo.comments.length)
+
   console.log(photo);
 };
 
